cce2db6d98dc65f29ce9e8b9b36336834bff859c
unknown <yuanyuanjiang89@gmail.com>
2026-01-13 17:08:32 +0100

    connect to MariaDB

---
+++ DXApplication1.Module/BusinessObjects/ORMDataModel1Code/Customer.cs
@@ -0,0 +1,22 @@
+﻿using DevExpress.Data.Filtering;
+using DevExpress.Persistent.Base;
+using DevExpress.Persistent.BaseImpl;
+using DevExpress.Xpo;
+using DevExpress.Xpo.Metadata;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Reflection;
+
+namespace DXApplication1.Module.BusinessObjects.Database { 
+
+    [DefaultClassOptions]
+    public partial class Customer
+    {
+        public Customer(Session session) : base(session) { }
+        public override void AfterConstruction() { base.AfterConstruction(); }
+
+
+    }
+
+}
---
+++ DXApplication1.Module/BusinessObjects/ORMDataModel1Code/Invoice.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using DevExpress.Xpo;
+using DevExpress.Xpo.Metadata;
+using DevExpress.Data.Filtering;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Reflection;
+using DevExpress.Persistent.Base;
+
+namespace DXApplication1.Module.BusinessObjects.Database
+{
+    [DefaultClassOptions]
+    public partial class Invoice
+    {
+        public Invoice(Session session) : base(session) { }
+        public override void AfterConstruction() { base.AfterConstruction(); }
+    }
+
+}
